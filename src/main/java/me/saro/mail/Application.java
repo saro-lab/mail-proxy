f13d5619015103d0a5ac8c912885683545b8348b
PARK Yong Seo <j@saro.me>
2019-05-06 03:50:59 +0900

    temp application

--- src/main/java/me/saro/mail/Application.java
+++ src/main/java/me/saro/mail/Application.java
@@ -2,9 +2,14 @@ package me.saro.mail;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
 
+@EnableAutoConfiguration
 @SpringBootApplication
+@ComponentScan({"me.saro.mail", "me.saro.mail.*"})
 public class Application implements CommandLineRunner {
 
 	public static void main(String[] args) {
--- src/main/java/me/saro/mail/DBConfig.java
+++ src/main/java/me/saro/mail/DBConfig.java
@@ -3,8 +3,10 @@ package me.saro.mail;
 import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.vendor.HibernateJpaSessionFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
 @Configuration
@@ -13,12 +15,11 @@ public class DBConfig {
 
     @Bean
     public DataSource dataSource() {
-        var url = "jdbc:h2:db/ldap";
         var conf = new HikariDataSource();
         conf.setDriverClassName("org.h2.Driver");
-        conf.setJdbcUrl(url);
-        conf.setUsername("ldap-user");
-        conf.setPassword("ldap-pass");
+        conf.setJdbcUrl("jdbc:h2:~/data.db/mail");
+        conf.setUsername("user");
+        conf.setPassword("pass");
         return conf;
     }
 }
