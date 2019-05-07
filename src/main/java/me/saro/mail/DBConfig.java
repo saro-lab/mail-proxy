74869986ff53c9b3bf0cefa2e0b700a71abbdba0
PARK Yong Seo <j@saro.me>
2019-05-08 02:57:38 +0900

    custom path

--- src/main/java/me/saro/mail/DBConfig.java
+++ src/main/java/me/saro/mail/DBConfig.java
@@ -1,6 +1,7 @@
 package me.saro.mail;
 
 import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -11,11 +12,14 @@ import javax.sql.DataSource;
 @EnableTransactionManagement
 public class DBConfig {
 
+    @Value("${data.path}")
+    private String dataPath;
+
     @Bean
     public DataSource dataSource() {
         var conf = new HikariDataSource();
         conf.setDriverClassName("org.h2.Driver");
-        conf.setJdbcUrl("jdbc:h2:./mail-proxy-data/mail");
+        conf.setJdbcUrl("jdbc:h2:"+dataPath+"/mail");
         conf.setUsername("user");
         conf.setPassword("pass");
         return conf;
