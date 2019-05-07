a09cf3c574cd9b15ad2ba686b89db2929355bd53
PARK Yong Seo <j@saro.me>
2019-05-08 03:08:01 +0900

    custom path

--- src/main/java/me/saro/mail/DBConfig.java
+++ src/main/java/me/saro/mail/DBConfig.java
@@ -1,6 +1,7 @@
 package me.saro.mail;
 
 import com.zaxxer.hikari.HikariDataSource;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,18 +9,30 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
 
+@Slf4j
 @Configuration
 @EnableTransactionManagement
 public class DBConfig {
 
-    @Value("${data.path}")
-    private String dataPath;
+    @Value("${data.path}/mail")
+    private String dbfile;
 
     @Bean
     public DataSource dataSource() {
+
+        String jdbcUrl;
+        if (!(dbfile.startsWith("./") || dbfile.startsWith("~/"))) {
+            jdbcUrl = "jdbc:h2:"+dbfile;
+        } else {
+            jdbcUrl = "jdbc:h2:file:"+dbfile;
+        }
+
+        log.info("h2 db file path : " + dbfile);
+        log.info("h2 db jdbcUrl : " + jdbcUrl);
+
         var conf = new HikariDataSource();
         conf.setDriverClassName("org.h2.Driver");
-        conf.setJdbcUrl("jdbc:h2:"+dataPath+"/mail");
+        conf.setJdbcUrl(jdbcUrl);
         conf.setUsername("user");
         conf.setPassword("pass");
         return conf;
